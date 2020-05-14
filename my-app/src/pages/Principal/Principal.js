5073753a116bbf41f49bde75984f4710b9f6f0f3
Ian Xavier <ian.wanderleyx@gmail.com>
2020-05-14 15:26:33 -0300

    adicionando principal

--- my-app/src/pages/Principal/Principal.js
+++ my-app/src/pages/Principal/Principal.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './styles.css';
-
-export default function Principal(){
-    return(
-        <div className='principal h-100'>
-            
-        </div>
-    );
-}
+import React, { Component } from 'react';
+import Header from './components/Header/Header.js';
+import Body from './components/Body/Body.js';
+import './style.css';
+const Principal = () => (
+<div className="App">
+      <Header />
+      <Body />
+    </div>
+);
+export default Principal;
