b46289a4b2de1f5adbdf13a6093a9d69197ceebe
Maria Caneschi <mariaclaracaneschi15@gmail.com>
2020-05-15 19:08:47 -0300

    página de cadastro em andamento

--- my-app/src/pages/Cadastro/Cadastro.js
+++ my-app/src/pages/Cadastro/Cadastro.js
@@ -1,14 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './styles.css';
 
 
-
 export default function Cadastro() {
-
     return (
         <div className="cadastro h-100">
-            <div className="bloco_cadastro d-flex justify-content-center flex-column">
-            </div>   
+            <form className="form d-flex justify-content-center flex-column align-items-center">
+                <p>Para criar sua conta preencha o formulário abaixo e clique em enviar!</p>
+                <input
+                    className="form-field" 
+                    type="text" 
+                    name="nome"
+                    placeholder="Digite seu nome"
+                    pattern="[A-Za-zÀ-ú ']{5,}"
+                    required="required"
+                    />
+                <input
+                    className="form-field" 
+                    type="email" 
+                    name="email"
+                    placeholder="Digite seu e-mail"
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                    required="required"
+                />
+                <input
+                    className="form-field" 
+                    type="password" 
+                    name="senha"
+                    placeholder="Crie uma senha"
+                    pattern=".{6,}"
+                    required="required"
+                />
+                <input type="submit" className="button"/>
+            </form>
         </div>
     );
 }
