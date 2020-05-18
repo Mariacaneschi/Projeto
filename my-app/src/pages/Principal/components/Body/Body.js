96305595f8eab3f788e9fd8c2afc51c2bc46b8e7
Ian Xavier <ian.wanderleyx@gmail.com>
2020-05-18 11:47:26 -0300

    Correção das imagens e do tamanho da página

--- my-app/src/pages/Principal/components/Body/Body.js
+++ my-app/src/pages/Principal/components/Body/Body.js
@@ -5,9 +5,9 @@ const Body = () => (
     <div class="central">
     <div id="divrup">
    <div id="meowth">
-   <img id="mm" src="https://lh3.googleusercontent.com/j_HSrTe4FJp5hTYJAeRzUD01jO5HXOS-zzaau8DYk58zsY71PvIB__hEq5tBP0-_LNh0rM39CJPMbG0jGed0b8dxa0wak7wsk_r0TIn74N11cONm5ewlfDVSzGcClYtmjv003ZHnUmAoGsXXhOGiqZVpGdR9U8X9C7oswNPVlR20zeeqJjq6PARa-Ke7LwxJNdPFDqGfRlI_g6ZzD8qFXM9BxUZ-WuWMfo7AQMQcvopLcVUVn8MI230PLFAS1eKxVencUTIhvYPlCdBSwadBAkOPPUUn33FFrIBhq-ruSPwsimF41EnO6AM4PQ0Fmvav-siEx5Jiz5NvX7N1rILXUqd-EhYOrVOpVaKQ26fQa-BZrWJ_U7fdeAtmxETP2ZmbfKjMXRbCG-T4qY2xYBZOuKhfcwjfXGt1rLVVoQlYBxIYlpDQeU_Q0p2Qj1EiSgEYogbFOfxgWDYB03twp1vs7dLItRg6YW76zjBMVbcjHo4S0pdM1edmM39cQWNqZzx6-adivCz8odwO_g3Zi2eFoqoPRGuIWu7MY9QmpyRjM0TYLEbD0PR6jMuDIfO361k70rsNg55Tg2DGpbJVWomrMmfPCwWs6td79Wv_UhO21AYjS8x82hJUainFychtstlL_q3OwMy1Zy3TX271VyGL4xi7cxQBcHYo8GksqfcRL_zb8cLr310TTXCI9n89hw=w672-h920-no?authuser=0"></img>
+   <img id="mm" src="https://i.imgur.com/DghKK7X.png"></img>
  <div id="balão">
-     <img id="balao" src="https://lh3.googleusercontent.com/62suHZj0Lh4DKgPNSc4Y9FUJ033k57rJUKRkRcMkTm8XGx4rzh5DBJnO0J1zhC7Tg6LgphAdY-gwye1h8foUoepMsZnZMb-vf7eBJpEbvfpS9J8B0bNdMJVNSM91Rna7OLuRWWQmXbTBHDzaZZuHCrVHfXQLdJJoeW2XHxbiuhIzTkPnaQB0hFLEpBABGcNAKcII4ve_mKjF7NP_ZlUePhkCOSPOXr-cAL5LyvVIbD0Z6lA9EQdhpLfYDmGl5gM7ZSEelO3sx-bfKyBHc0q_SogNY5KTLWyQCYykIBiy4yzLgN38xXlTDvVxE_qMsWP54WlMYrEjeCfKr2Ix9M2LiNAMK2fAaryjQYqzxtZUqVsMMo17M0Fn8ZBtwVapFfDTf08Q1osMgUzVwc1gtntVO2QjUI0NPtlh5-KrVy_B1Ijay34F3PqFLgh3Mmzt2WF_VbB4kOFrVi6UafqCvm0TYqGWOqmRMFqcUY4OJc1Wc00HXnfsVxvgekVWXWdDYZXdLcXoR0pYeZaYWWrmis3Bspr23i4ikZSIG0CbNW2wQd8r7U6q5mjeu-eTDJ09OrUxmifZX0pldHiU5xpN0jetXspmjsYsE03-94ks9SVpo2pXccyLc9Bifr9T2zHGH7R7jnuXd6CcyfVzQ01AL-wgV6fnAJQYSPFBtWCoRWF_o-sDscs9_SbVExpXjbo4Ww=w900-h820-no?authuser=0"></img>
+     <img id="balao" src="https://i.imgur.com/l0w6gLa.png"></img>
      </div>
      </div>
      </div>
