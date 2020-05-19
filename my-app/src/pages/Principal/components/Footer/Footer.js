4ceb62fd1d15d081ba5a17cff6fd4b566dafad16
Ian Xavier <ian.wanderleyx@gmail.com>
2020-05-19 15:16:58 -0300

    Muita coisa na pagina princpal

---
+++ my-app/src/pages/Principal/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+const Footer = () => (
+    <footer id="footer"> 
+    <div id="img1">
+        <div id="menu">
+            Desenvolvido pela equipe 2 do processo seletivo cpe.
+        </div>
+        <a href="https://cpejr.com.br/site/">
+        <img id="logocpe" src="https://cpejr.com.br/site/wp-content/uploads/2018/01/Logo-Branca-CPE-200x69.png">
+        </img>
+        </a>
+    </div>
+    </footer>
+)
+export default Footer;
--- my-app/src/pages/Principal/components/Header/Header.js
+++ my-app/src/pages/Principal/components/Header/Header.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 import './style.css';
 const Header = () => ( 
 <header id="main-header"> 
 <div id="img"> </div>
+<Button variant="contained" id="suporte">
+    <div id="suportetexto">Suporte</div>
+    </Button>
+    <Link to="/case">
+<Button variant="contained" id="case">
+    <div id="pokemonstexto">Pokemons</div>
+    </Button></Link>
+    <Link to="/pokedex">
+<Button variant="contained" id="pokedex">
+    <div id="pokedextexto">Pokedex</div>
+    </Button></Link> 
 <div id="titulo">Pokedex online</div></header> 
 );
 
