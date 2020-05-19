2499b2cf8ab6f6e4a987f88e5cfb48ceac1bdc7a
Maria Caneschi <mariaclaracaneschi15@gmail.com>
2020-05-19 10:03:52 -0300

    página de cadastro pronta

--- my-app/src/pages/Cadastro/Cadastro.js
+++ my-app/src/pages/Cadastro/Cadastro.js
@@ -1,78 +1,84 @@
 import React from 'react';
 import './styles.css';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 export default function Cadastro() {
     const history = useHistory();
-    function redireciona(){
+    function redireciona() {
         history.push('/');
     }
 
     return (
         <div className="cadastro h-100">
-            <div className="mensagem">
-                <h1>Bem vindo à Pokedex</h1>
-            </div>
-            <form onSubmit={() => redireciona()}  className="form d-flex justify-content-center flex-column">
-
-                <label>Digite seu nome completo</label>
-                <div className="inputs">
-                    <input
-                        className="form-field"
-                        type="text"
-                        name="nome"
-                        placeholder="Digite seu nome"
-                        pattern="[A-Za-zÀ-ú ']{5,}"
-                        required="required"
-                    />
+            <div className="teste d-flex justify-content-center flex-column">
+                <div className="mensagem d-flex justify-content-center flex-column align-items-center">
+                    Bem vindo à sua nova Pokedex!
+                <br />
+                    <p>Aqui você poderá realizar o cadastro para descobrir um novo mundo com seus Pokemóns preferidos!</p>
+                    <h6>Por favor, preencha os campos abaixo com seus dados corretamente e depois de enviar poderá fazer o login na página principal e poderá conhecer as experiências maravilhosas que o aguardam.</h6>
                 </div>
 
-                <label>E-mail</label>
-                <div className="inputs">
-                    <input
-                        className="form-field"
-                        type="email"
-                        name="email"
-                        placeholder="Digite seu e-mail"
-                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                        required="required"
-                    />
-                </div>
+                <form onSubmit={() => redireciona()} className="form d-flex justify-content-center flex-column">
 
-                <label>Crie uma senha (mínimo 6 caracteres)</label>
-                <div className="inputs">
-                    <input
-                        className="form-field"
-                        type="password"
-                        name="senha"
-                        placeholder="Senha"
-                        pattern=".{6,}"
-                        required="required"
-                    />
-                </div>
-
-                <label>Escolha sua função</label>
-                <div className="radio">
-                    <div className="form-field">
+                    <label>Digite seu nome:</label>
+                    <div className="inputs">
                         <input
-                            type="radio"
-                            name="funcao"
-                            value="treinador"
+                            className="form-field"
+                            type="text"
+                            name="nome"
+                            placeholder="Nome"
+                            pattern="[A-Za-zÀ-ú ']{5,}"
                             required="required"
-                        />  Treinador
-                        <br></br>
-                        <input
-                            type="radio"
-                            name="funcao"
-                            value="professor"
-                            required="required"
-                        />  Professor
+                        />
                     </div>
-                </div>
-                <div className="inputs">
-                    <input type="submit" className="button"/>
-                </div>
-            </form>
+
+                    <label>Digite seu e-mail:</label>
+                    <div className="inputs">
+                        <input
+                            className="form-field"
+                            type="email"
+                            name="email"
+                            placeholder="E-mail"
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                            required="required"
+                        />
+                    </div>
+
+                    <label>Crie uma senha (mínimo 6 caracteres):</label>
+                    <div className="inputs">
+                        <input
+                            className="form-field"
+                            type="password"
+                            name="senha"
+                            placeholder="Senha"
+                            pattern=".{6,}"
+                            required="required"
+                        />
+                    </div>
+
+                    <label>Escolha sua função:</label>
+                    <div className="radio">
+                        <div className="form-field">
+                            <input
+                                type="radio"
+                                name="funcao"
+                                value="treinador"
+                                required="required"
+                            />  Treinador
+                        <br></br>
+                            <input
+                                type="radio"
+                                name="funcao"
+                                value="professor"
+                                required="required"
+                            />  Professor
+                    </div>
+                    </div>
+                    <div className="inputs">
+                        <input type="submit" className="button" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
